44ff7e692c71c44c9e2656d75b0f3893f39a803e
scottmai <scottmai@usc.edu>
2022-02-06 11:22:24 -0800

    rewrite

---
+++ snap/Public/start.js
@@ -0,0 +1,8 @@
+// -----JS CODE-----
+//@input Component.ScriptComponent LearnScript
+
+if(Math.random() < .5){
+    script.LearnScript.api.learnFirst();
+} else{
+    script.LearnScript.api.gameFirst();
+}
